2805364fe2dc277cc1f455a94b1c18731961f2fa
Laís Agapito <113560382+LaisAgapito@users.noreply.github.com>
2023-09-25 20:51:10 -0300

    Add: resposta

---
+++ Migrations/20230925230425_veiculo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace APIExemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class veiculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    IdVeiculo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(type: "longtext", nullable: false),
+                    ano = table.Column<int>(type: "int", nullable: false),
+                    placa = table.Column<string>(type: "longtext", nullable: false),
+                    modelo = table.Column<string>(type: "longtext", nullable: false),
+                    marca = table.Column<string>(type: "longtext", nullable: false),
+                    cor = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.IdVeiculo);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
